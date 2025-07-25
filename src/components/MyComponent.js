5f55adf4e306fb49abd6a975ff441853ad73e86a
PhanTrungDuc <phantrungduc2522005@gmail.com>
2025-07-25 18:54:41 +0700

    react basic 3

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,10 +6,18 @@ class MyComponent extends React.Component {
         address: 'Vietnam',
         age: 22
     }
+
+    handleClick(event) {
+        alert("Button clicked");
+        // console.log("State's name:", this.state.name);
+    }
+
     render() {
         return (
             <div>
                 My name is {this.state.name}
+                <br />
+                <button onClick={this.handleClick}>Click me</button>
             </div>
         );
     }
