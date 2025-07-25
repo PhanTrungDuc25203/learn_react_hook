d4ef39ce39f96fa61ccd39ea7074b5db5e08db18
PhanTrungDuc <phantrungduc2522005@gmail.com>
2025-07-26 00:49:50 +0700

    react basic 5

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,11 +4,19 @@ import DisplayInfo from "./DisplayInfo";
 
 class MyComponent extends React.Component {
 
+    state = {
+        listUsers: [
+            { id: 1, name: "Piscean", age: 22 },
+            { id: 2, name: "Piscean2", age: 23 },
+            { id: 3, name: "Piscean3", age: 24 },
+        ]
+    }
+
     render() {
         return (
             <div>
                 <UserInfo />
-                <DisplayInfo name="Duc" age="22" />
+                <DisplayInfo listUsers={this.state.listUsers} />
             </div>
         );
     }
