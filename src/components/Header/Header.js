e974a03cb23f906a2ddc6f8d9c1a0913bd07ebb2
PhanTrungDuc <phantrungduc2522005@gmail.com>
2025-07-28 12:39:10 +0700

    add and define routes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Header.scss';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">Ecami</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/user" className="nav-link">User</Link>
+                        <Link to="/Admin" className="nav-link">Admin</Link>
+                        {/* <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/user">User</Nav.Link>
+                        <Nav.Link href="/admin">Admin</Nav.Link> */}
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Logout
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Help</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Profile
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
