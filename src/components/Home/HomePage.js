6d9f83475a9a4d1d5bb71352331cded71ab306ee
PhanTrungDuc <phantrungduc2522005@gmail.com>
2025-07-29 20:38:24 +0700

    css cooperation customer part

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,14 +1,27 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Hls from "hls.js";
 import homepageBackgroundVideo from "../../assets/homepage_background_video.m3u8";
 import "./HomePage.scss";
 
+const customerLogos = [
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/6728923eb22e7ee7c15e8396_calendly.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/6728910a1c16455593c27afa_citizen_m%201.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890fcdec577bb01f7b5f8_loccitane.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890ec4a4f53e7ce97a439_wetransfer.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890d44ff06c03b9169b96_slack.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890ca58ffc8edfb664691_webflow.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890be48319096ffeb6bfe_zapier.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890b1d5a69d1620412cf7_barrys.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890a3d27752eaed89aa47_hubspot.svg",
+  "https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/67289096a54485774b102f83_hermes.svg",
+];
+
 const HomePage = () => {
   const videoRef = useRef(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     const video = videoRef.current;
-
     if (video) {
       if (video.canPlayType("application/vnd.apple.mpegurl")) {
         video.src = homepageBackgroundVideo;
@@ -22,8 +35,19 @@ const HomePage = () => {
     }
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev === 0 ? 1 : 0));
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const firstGroup = customerLogos.slice(0, 5);
+  const secondGroup = customerLogos.slice(5, 10);
+
   return (
     <div className="homepage-container">
+      {/* Video banner */}
       <div className="background-banner">
         <div className="left-half">
           <video
@@ -46,131 +70,33 @@ const HomePage = () => {
             </p>
           </div>
           <button className="homepage-banner-button">
-            Get started-it's free
+            Get started - it's free
           </button>
         </div>
       </div>
-      <div className="container">
-        <div className="customers_component">
-          <div className="w-dyn-list">
-            <div role="list" className="customers_row upper w-dyn-items">
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/6728923eb22e7ee7c15e8396_calendly.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/6728910a1c16455593c27afa_citizen_m%201.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890fcdec577bb01f7b5f8_loccitane.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890ec4a4f53e7ce97a439_wetransfer.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890d44ff06c03b9169b96_slack.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-            </div>
-          </div>
 
-          <div className="w-dyn-list">
-            <div role="list" className="customers_row lower w-dyn-items">
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890ca58ffc8edfb664691_webflow.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890be48319096ffeb6bfe_zapier.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890b1d5a69d1620412cf7_barrys.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/672890a3d27752eaed89aa47_hubspot.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
-              <div
-                role="listitem"
-                className="customers_logo-container w-dyn-item"
-              >
-                <img
-                  src="https://cdn.prod.website-files.com/6718da5ecf694c9af0e8d5d7/67289096a54485774b102f83_hermes.svg"
-                  loading="lazy"
-                  alt=""
-                  className="integrations_carousel-item-logo"
-                />
-              </div>
+      {/* Customers section */}
+      <div className="coop-customer-container">
+        <div className="customers_component">
+          {[firstGroup, secondGroup].map((group, index) => (
+            <div
+              key={index}
+              className={`customers_row slide ${
+                currentSlide === index ? "active" : "inactive"
+              }`}
+            >
+              {group.map((logo, i) => (
+                <div key={i} className="customers_logo-container">
+                  <img
+                    src={logo}
+                    loading="lazy"
+                    alt=""
+                    className="integrations_carousel-item-logo"
+                  />
+                </div>
+              ))}
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
